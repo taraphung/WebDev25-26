16f1397139cf6db215be133362db1ea10a4d1446
Tara Phung <tarap15@nycstudents.net>
2026-01-06 18:02:56 +0000

    x

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function cat(){
+    document.getElementById("highlight").innerHTML = "This is a picture of a gray cat.";
+}
+
+function dog(){
+    document.getElementById("highlight").innerHTML = "Here is a picture of a brown dog.";
+}
+
+function hamster(){
+    document.getElementById("highlight").innerHTML = "This is a picture of an orange hamster.";
+}
+
+function parrot(){
+    document.getElementById("highlight").innerHTML = "Here is a picture of a green parrot.";
+}
+
+function swan(){
+    document.getElementById("highlight").innerHTML = "This is a picture of a white swan.";
+}
