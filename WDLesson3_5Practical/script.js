b48a05b19acdd6375f30963364212bb296ac1bc9
Tara Phung <tarap15@nycstudents.net>
2026-01-16 20:29:09 +0000

    sa

--- WDLesson3_5Practical/script.js
+++ WDLesson3_5Practical/script.js
@@ -4,11 +4,19 @@
       3) Display the results in the appropriate element
 */
 function recArea(){
-
+      let len= parseFloat(document.getElementById("l").value);
+      let wid= parseFloat(document.getElementById("w").value);
+      let op= document.getElementById("output");
+      let A= len*wid;
+      op.innerHTML=A;
 }
 
 function recPerimeter(){
-
+      let len= parseFloat(document.getElementById("l").value);
+      let wid= parseFloat(document.getElementById("w").value);
+      let op= document.getElementById("output");
+      let A= len+wid;
+      op.innerHTML=A;
 }
 
 
@@ -20,11 +28,19 @@ function recPerimeter(){
 let pi = 3.1415926;
 
 function cirArea(){
-
+      let pi = 3.1415926;
+      let rad= parseFloat(document.getElementById("r").value);
+      let out= document.getElementById("output");
+      let B= pi*rad**2;
+      out.innerHTML=B;
 }
 
 function cirPerimeter(){
-
+      let pi = 3.1415926;
+      let rad= parseFloat(document.getElementById("r").value);
+      let out= document.getElementById("output");
+      let B= 2*pi*rad;
+      out.innerHTML=B;
 }
 
 /* Challenge Bonus: Complete the area and perimeter functions below for the triangle.  Guidelines,
