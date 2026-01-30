11d3f5ceeb867da8b8b42024e7a7603d7f8ba85e
Tara Phung <tarap15@nycstudents.net>
2026-01-30 17:58:05 +0000

    qq

--- WDLesson3_8Practical/script.js
+++ WDLesson3_8Practical/script.js
@@ -2,20 +2,21 @@
 //  For each challenge fix the error and explain the correction in the comment
 
 
-/* Challenge 4: Does the function name match the event handler referenced in the event listener? */
-function volume(){
+/* Challenge 4: Does the function name match the event handler referenced in the event listener? */   /*function name is not volume(), it is cylinder()*/
+function cylinder(){
 
-  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */
-  let r = parseInt(document.getElementBy("r").value);
-  let height = parseInt(document.getElementbyId("height").value);
+  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */   /*line 9: document.getElementById is missing "Id" and line 10: capitalize B in "by". Change id name and let value based on index.html */
+  let r = parseInt(document.getElementById("r").value);
+  let h = parseInt(document.getElementById("h").value);
 
-  /* Challenge 6: Does the variable output correctly create a reference to the output container? */
-  let output = document.getElementById("output").value;
+  /* Challenge 6: Does the variable output correctly create a reference to the output container? */     /*.value is not needed here*/
+  let output = document.getElementById("output");
 
-  /* Challenge 7: Is the following calculation for volume of a cylinder correct? */
-  let v = math.PI * math.pow(r,2) / h;
+  /* Challenge 7: Is the following calculation for volume of a cylinder correct? */   /* The "M" in Math for "math.PI" and "math.pow" needs to be capitalized. Equation is pi * (r**2) *h, not divided by h. */
+  let v = Math.PI * Math.pow(r,2) * h;
+  v= v.toFixed(2);
 
-  /* Challenge 8: Are there any errors in displaying the output? */
-  output.innerHTML = `Volume of the cylinder is ${v} 
+  /* Challenge 8: Are there any errors in displaying the output? */    /*Missing backtick and semicolon at the end of code. */ 
+  output.innerHTML = `Volume of the cylinder is ${v}`; 
 
 }
