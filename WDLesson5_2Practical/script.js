872dafb5d5c10650cb0d6bc5defe3e9d601e7420
Tara Phung <tarap15@nycstudents.net>
2026-03-12 16:59:58 +0000

    y

--- WDLesson5_2Practical/script.js
+++ WDLesson5_2Practical/script.js
@@ -17,6 +17,7 @@ Now that you have the template, use a for loop to produce all five pizza cards.
 
 //init() function is called when the page loads in index.html
 function init(){
+   let output=document.getElementById("output");
    let build="";
    for (let i=1; i<=5; i++){
       let price=Math.floor(Math.random()*(35-15+1)+15);
