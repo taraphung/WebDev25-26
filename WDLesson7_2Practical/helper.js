506ac7c3fba27fce930915ed5cdf982f5f385287
Tara Phung <tarap15@nycstudents.net>
2026-05-14 17:19:11 +0000

    x

---
+++ WDLesson7_2Practical/helper.js
@@ -0,0 +1,37 @@
+// get() returns the element using document.getElementById().
+function get(id){
+  return document.getElementById(id);
+}
+
+// Challenge 3: Create the function showMap() that displays the map for a location [lat, lon]
+function showMap(lat,lon){
+  let location = [lat, lon];
+  if(!mapObj){
+      mapObj = L.map("map");
+  } 
+  let map = mapObj.setView(location, 14);// [lat, lon], zoom
+
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(map);
+
+  let marker = L.marker(location).addTo(map);// places marker on map
+}   
+  
+
+
+// Challenge 4: Create the function card() to generate an appropriate info card with the button to view map
+function card( info ){ 
+  let build = `<div class="card fitted">
+                  <h3>${info.collision_id}</h3>
+                  <hr>
+                  <h4>Borough: ${info.borough}</h4>
+                  <p>Location: ${info.on_street_name}</p>
+                  <h5>Vehicle: ${info.vehicle_type_code1}</h5>`;
+                  if(info.latitude && info.longitude){
+                    build += `<input type="button" value="Map" onclick="showMap( ${info.latitude}, ${info.longitude} )">`;
+                  }
+    build +=    `</div>`;
+  return build;
+}
